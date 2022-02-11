02c02466262f522ef2b462d9ecc2321ea6f85ba2
LAPTOP-I8DPA09K\ferto <fertorresaavedra@gmail.com>
2022-02-10 21:05:30 -0500

    baje lo de el github

---
+++ Main.java
@@ -0,0 +1,55 @@
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
+class Main {
+    private static String[] lista = new String[726];
+    private static String nombre = "urls.txt";
+    private static int i = 0;
+    private static String[] listado = new String[726];
+    public static void main(String[] args) {
+        try{
+
+            File f;
+            FileReader lectorArchivo;
+
+
+            f = new File(nombre);
+
+
+            lectorArchivo = new FileReader(f);
+
+
+            BufferedReader br = new BufferedReader(lectorArchivo);
+
+            
+
+
+
+
+            for(i=0;i<726;i++)
+            {
+                lista[i]=br.readLine();
+                
+            }
+
+            br.close();
+
+            lectorArchivo.close();
+
+
+
+        }catch(IOException e){
+            System.out.println("Error:"+e.getMessage());
+        }
+        for(i=0;i<726;i++){
+            System.out.println("-"+lista[i]);
+        }
+        
+    }
+
+}
